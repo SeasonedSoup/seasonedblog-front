4a538944345544c4385e8212b5778f9560af4c0e
SeasonedSoup <emmetteagoncillo1@gmail.com>
2026-03-19 11:55:57 +0800

    Integrate AuthProvider to main.jsx + improve struc
    structure of Homepage and copied signupform from
    seasonedblog write repo

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
+import {useAuth} from "./AuthToken/AuthContext"
 
 function Homepage() {
     const [posts, setPosts] = useState([]);
     const navigate = useNavigate();
-
+    const {user, loading} = useAuth();
 
     async function fetchPosts() {
             const url = "http://localhost:8000/api/publishedPosts"
@@ -18,8 +19,6 @@ function Homepage() {
             const posts = await response.json();
            
             setPosts(posts);
-            setUnpublishedPosts(unpublished);
-            setLoading(false)
         }
     
     fetchPosts();
@@ -27,9 +26,26 @@ function Homepage() {
     function visitPost(id, post) {
         navigate(`/post/${id}`, {state: {post}});
     }
+
+    if (loading) return <div>Loading...</div>;
+
+    if (!user) return (
+        <>
+            <h1>PLEASE LOG IN IF YOU HAVE AN ACCOUNT</h1>
+            <form action="">
+                <label htmlFor="username">Username: </label>    
+                <input type="text" id="username" name="username"/>
+                <label htmlFor="password">Password: </label>
+                <input type="password" id="password" name="password"/>
+            </form>
+            <a href="/signup">Sign up Here</a>
+        </>
+    )
+    
+    
     return (
         <>
-            <h1>HELLO WELCOME TO SEASONED BLOG!!</h1>
+            <h1>HELLO WELCOME TO SEASONED BLOG!, {user?.username}</h1>
             <a>VIEW THE BLOGS HERE!</a>
             {posts.map((post) => {
                 return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {RouterProvider, createBrowserRouter } from 'react-router'
+import {AuthProvider} from "./components/AuthToken/AuthProvider"
 
 import Homepage from './components/Homepage'
 import Post from './components/Post'
+import SignUpForm from './components/SignUpForm'
+
 const router = createBrowserRouter([
     {
         path: "/",
@@ -12,11 +15,17 @@ const router = createBrowserRouter([
     {
         path: "post/:id",
         element: <Post/>
+    },
+    {
+        path: "/signup",
+        element: <SignUpForm/>
     }
 ])
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <RouterProvider router={router}></RouterProvider>
+        <AuthProvider>
+             <RouterProvider router={router}></RouterProvider>
+        </AuthProvider>
     </StrictMode>,
 )
